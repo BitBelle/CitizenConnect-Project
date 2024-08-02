bf07de5fc0abba1efd71b5ed1ec40886d53f3087
BitBelle <wainainamercy432@gmail.com>
2024-08-03 00:06:11 +0300

    Modified the Tables

--- CitizenConnect-Backend/Database/Tables/Polls/pollOptions.sql
+++ CitizenConnect-Backend/Database/Tables/Polls/pollOptions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE PollOptions(
     optionId VARCHAR(255) PRIMARY KEY,
-    pollQuestionId VARCHAR(255) NOT NULL,
-    optionText VARCHAR(255) NOT NULL,
+    pollQuestionId VARCHAR(255) ,
+    optionText VARCHAR(255) ,
     isDeleted INT DEFAULT 0
     
     FOREIGN KEY (pollQuestionId) REFERENCES PollQuestion(pollQuestionId) ON DELETE CASCADE
--- CitizenConnect-Backend/Database/Tables/Polls/pollQuestion.sql
+++ CitizenConnect-Backend/Database/Tables/Polls/pollQuestion.sql
@@ -2,9 +2,9 @@ USE CitizenConnect360
 
 CREATE TABLE PollQuestion(
     pollQuestionId VARCHAR(255) PRIMARY KEY,
-    userId VARCHAR(255) NOT NULL,
-    pollQuestion VARCHAR(255) NOT NULL,
-    pollStatus VARCHAR(255) NOT NULL,
+    userId VARCHAR(255) ,
+    pollQuestion VARCHAR(255) ,
+    pollStatus VARCHAR(255),
     isDeleted INT DEFAULT 0,
 
     FOREIGN KEY (userId) REFERENCES Users(userId) ON DELETE CASCADE
