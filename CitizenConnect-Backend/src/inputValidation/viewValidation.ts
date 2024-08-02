5050ded0d40f66170e8edc200498ea22c168020d
BitBelle <wainainamercy432@gmail.com>
2024-08-03 00:16:02 +0300

    Modified the viewSchema

--- CitizenConnect-Backend/src/inputValidation/viewValidation.ts
+++ CitizenConnect-Backend/src/inputValidation/viewValidation.ts
@@ -4,7 +4,7 @@ import Joi from 'joi'
 export const ViewSchema = Joi.object({
     
     userId: Joi.string().required(),
-    userName: Joi.string().required(),
+    // userName: Joi.string().required(),
     viewContent: Joi.string().required()
 
 })
